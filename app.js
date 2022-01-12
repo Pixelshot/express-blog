bbead15e21b0cef288b60fb1bfb4455865f63bd4
Pixelshot <pixelshot8@gmail.com>
2022-01-12 15:08:56 +0800

    Connected to MongoDB database. Moving on to Requests

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+const dbURI =
+  'mongodb+srv://pixelshot:pixelshot123@clusterninjaexpress.wyvhm.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  // Only listen for requests AFTER connection to the database is established
+  .then((data) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // express app
 const app = express();
@@ -18,6 +31,7 @@ app.use(cors());
 // Express will run through these routes from TOP to BOTTOM
 
 // Middleware & Static Files
+
 // The server protects our files automatically from users in a browser. They can't just access our files anytime they want.
 // We have to specify what files should be allowed for public view.
 // Usually app.use() is used for middlewares.
@@ -30,31 +44,61 @@ app.use(morgan('dev'));
 // How to render our view
 // Use res.render(name of the file without the extension. extension has been declared at the top with app.set())
 app.get('/', (req, res) => {
-  const blogs = [
-    {
-      title: 'Yoshi finds eggs',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-    {
-      title: 'Mario finds stars',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-    {
-      title: 'How to defeat bowser',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-  ];
-  // Dynamic data is passed through to our front-end via object as a second argument in res.render()
-  res.render('index', {
-    title: 'Home',
-    blogs,
-  });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About',
   });
+});
+
+// mongoose and mongo sandbox routes
+// Blog Routes
+app.get('/blogs', (req, res) => {
+  // There is a sort method in mongoose.
+  // createdAt is coming from Blog Model
+  // - 1 = descending order. Newest to oldest
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', {
+        title: 'Home',
+        blogs: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Get all blogs
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
+
+// Get a single blog
+app.get('/single-blog', (req, res) => {
+  Blog.findById('61de6d6f48a4deeb3adf2ea4')
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
+
+// Add a blog
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'New Blog 2',
+    snippet: 'about my new blog',
+    body: 'more about my new blog',
+  });
+
+  // save() is a mongoose method
+  blog
+    .save()
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
 });
 
 app.get('/blogs/create', (req, res) => {
@@ -73,9 +117,31 @@ app.use((req, res) => {
 });
 
 // listen for requests
-app.listen(3000);
+// app.listen(3000);
 
 // === === === === === === === === === === === === === === === === === === === === === === === === ===
+// app.get('/', (req, res) => {
+//   const blogs = [
+//     {
+//       title: 'Yoshi finds eggs',
+//       snippet: 'Lorem ipsum dolor sit amet consectetur',
+//     },
+//     {
+//       title: 'Mario finds stars',
+//       snippet: 'Lorem ipsum dolor sit amet consectetur',
+//     },
+//     {
+//       title: 'How to defeat bowser',
+//       snippet: 'Lorem ipsum dolor sit amet consectetur',
+//     },
+//   ];
+//   // Dynamic data is passed through to our front-end via object as a second argument in res.render()
+//   res.render('index', {
+//     title: 'Home',
+//     blogs,
+//   });
+// });
+
 // app.get('/', (req, res) => {
 //   // res.send('<p>home page</p>');
 //   // use sendFile to serve our HTML page.
